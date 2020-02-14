269867ee9c80a05b234de517fd8cb52db180b623
Foxkrak <foxkrak@gmail.com>
2020-02-14 02:18:35 -0300

    Avanços na area de login delete e posts..

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('home', views.post_list, name='home'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/edit/<int:pk>', views.post_edit, name='post_edit'),
+    path('del/<int:pk>', views.post_del, name='post_del'),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
--- myblog/settings.py
+++ myblog/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
