c9eaac7565d18d9d25b84465a832bbebc4a32465
Foxkrak <foxkrak@gmail.com>
2020-02-16 21:08:29 -0300

    projeto praticamente concluido!

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post_tipo =[('AT','Atividades'),('PR','Projetos'),('SB', 'Sobre')]
